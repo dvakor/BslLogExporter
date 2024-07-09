dc0c9920450f9f40b66f7f83c79f007312a0f66d
Dmitriy Korolev <d.korolev@live.ru>
2024-07-09 10:18:36 +0700

    Добавить сведения о файле, в котором произошла ошибка

--- src/BslLogExporter.Core/LogReader/LogEntryParseException.cs
+++ src/BslLogExporter.Core/LogReader/LogEntryParseException.cs
@@ -4,8 +4,8 @@ namespace LogExporter.Core.LogReader
 {
     public class LogEntryParseException : Exception
     {
-        public LogEntryParseException(BracketsNodeValue node, string fileName, Exception innerException) 
-            : base($"Ошибка парсинга элемента: {node}, в файле {fileName}", innerException)
+        public LogEntryParseException(BracketsNodeValue node, string filePath, Exception innerException) 
+            : base($"Ошибка парсинга элемента: {node}, в файле {filePath}", innerException)
         {
             
         }
